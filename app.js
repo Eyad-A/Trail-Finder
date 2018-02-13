d461970a24ac6ce7afd717cd19e7116581463751
Eyad Alshafi <malshafi@yahoo.com>
2018-02-13 02:54:29 +0000

    Added authentication

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-var express =    require("express"),
-    app =        express(),
-    bodyParser = require("body-parser"),
-    mongoose =   require("mongoose"),
-    Trail =      require("./models/trail"),
-    Comment =    require("./models/comment"),
-    seedDB =     require("./seeds");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Trail       = require("./models/trail"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
     
 mongoose.connect("mongodb://localhost/trail_finder");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Get to the next screen",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // var trails = [
 //             {name: "Spring Creek", image: "https://farm6.staticflickr.com/5240/5901787178_5eef7e4134.jpg"},
@@ -29,14 +49,14 @@ app.get("/", function(req, res) {
 
 //INDEX
 app.get("/trails", function(req, res) {
-        //Get all trails from DB
-        Trail.find({}, function(err, allTrails) {
-            if (err) {
-                console.log(err);
-            } else {
-                 res.render("trails/index", {trails: allTrails});
-            }
-        });
+    //Get all trails from DB
+    Trail.find({}, function(err, allTrails) {
+        if (err) {
+            console.log(err);
+        } else {
+             res.render("trails/index", {trails: allTrails});
+        }
+    });
 });
 
 //CREATE
@@ -77,7 +97,7 @@ app.get("/trails/:id", function(req, res) {
 // Comments Routes
 //=================
 
-app.get("/trails/:id/comments/new", function(req, res) {
+app.get("/trails/:id/comments/new", isLoggedIn, function(req, res) {
     Trail.findById(req.params.id, function(err, trail) {
         if (err) {
             console.log(err);
@@ -89,7 +109,7 @@ app.get("/trails/:id/comments/new", function(req, res) {
     
 });
 
-app.post("/trails/:id/comments", function(req, res) {
+app.post("/trails/:id/comments", isLoggedIn, function(req, res) {
     Trail.findById(req.params.id, function(err, trail) {
         if (err) {
             console.log(err);
@@ -108,6 +128,50 @@ app.post("/trails/:id/comments", function(req, res) {
     });
 });
 
+//AUTH ROUTES
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/trails");
+        });
+    });
+});
+
+//Login
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/trails",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+//LOGOUT ROUTES
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/trails");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("TrailFinder has started!");
-})
+});
